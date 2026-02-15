abb66a6b4ec5c392ed4332b62e3254e29e67b823
nishant4671 <nishantkr4671@gmail.com>
2026-02-15 13:26:55 +0530

    good # Your branch is up to date with 'origin/main'.

--- phase_01/hash table and sets/medium/group_anagrams_49.py
+++ phase_01/hash table and sets/medium/group_anagrams_49.py
@@ -37,10 +37,20 @@
 
 # 1 <= strs.length <= 104
 # 0 <= strs[i].length <= 100
-# strs[i] consists of lowercase English letters.
+# strs[i] consists of lowercase English lett
 class Solution(object):
     def groupAnagrams(self, strs):
-        
 
+        groups = {}
+
+        for word in strs:
+            key = ''.join(sorted(word))
+
+            if key not in groups:
+                groups[key] = []
+
+            groups[key].append(word)
+
+        return list(groups.values())
 
 
